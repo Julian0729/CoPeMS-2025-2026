f27fb249432fc8742afb2436d5d4fd7ee59f878f
azadajacqueline <babyjacqueline.azada@unc.edu.ph>
2025-11-18 13:33:00 +0800

    applicant

--- copems-frontend/src/main.js
+++ copems-frontend/src/main.js
@@ -12,6 +12,7 @@ import App from './App.vue'
 
 // Composables
 import { createApp } from 'vue'
+import router from '@/router'
 
 // Styles
 import 'unfonts.css'
@@ -19,5 +20,6 @@ import 'unfonts.css'
 const app = createApp(App)
 
 registerPlugins(app)
+app.use(router)
 
 app.mount('#app')
